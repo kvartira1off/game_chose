90e099c37e6a15d2d3952b5caa92fab33b5df5c1
guntandrey <guntandrey@gmail.com>
2024-04-11 18:12:38 +0300

    diplay add

---
+++ script.js
@@ -0,0 +1,5 @@
+let container = document.querySelector('.container')
+let profile_btn = container.querySelector('.profile-btn')
+profile_btn.addEventListener('click', function(){
+    container.style.display = "none"
+})
