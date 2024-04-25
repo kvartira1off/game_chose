46817bd04534b4d5e3570649882f29fd8b8aa040
guntandrey <guntandrey@gmail.com>
2024-04-25 18:28:04 +0300

    all variables2

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let layers = document.querySelector('.layers')
+let main_screen = document.querySelector('.main-screen')
+let profile_screen = document.querySelector('.profile-screen')
+let wallet_screen = document.querySelector('.profile-screen')
+let help_screen = document.querySelector('.help-screen')
+
 let profile_btn = document.querySelector('.profile-btn')
 let wallet_btn = document.querySelector('.wallet-btn')
 let help_btn = document.querySelector('.help-btn')
 let filter_btn = document.querySelector('.filter-btn')
+let back_btn1 = profile_screen.querySelector('.back-btn')
+let back_btn2 = wallet_screen.querySelector('.back-btn')
+let back_btn3 = help_screen.querySelector('.back-btn')
+
 profile_btn.addEventListener('click', function(){
-    layers.style.display = "none"
+    main_screen.style.display = "none"
 })
 wallet_btn.addEventListener('click', function(){
-    layers.style.display = "none"
+    main_screen.style.display = "none"
 })
 help_btn.addEventListener('click', function(){
-    layers.style.display = "none"
+    main_screen.style.display = "none"
 })
 filter_btn.addEventListener('click', function(){
 })
